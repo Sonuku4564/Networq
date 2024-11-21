3dd714047fd535cda8a494621b3146f869a16599
unknown <Sonuku4564@gmail.com>
2024-11-21 17:46:16 +0530

    Successfully implemented protected Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,39 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./components/Layout/Layout"
 
 import HomePage from "./pages/HomePage"
 import SignupPage from "./pages/auth/SignupPage"
 import LoginPage from "./pages/auth/LoginPage"
-import { Toaster } from "react-hot-toast"
+import { Toaster , toast } from "react-hot-toast"
+import { useQuery } from "@tanstack/react-query"
+import { axiosInstance } from "./lib/axios"
 
 function App() {
+
+  const {data: authUser, isLoading} = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async ()=>{
+      try{
+        const res = await axiosInstance.get("/auth/me");
+        return res.data;
+      }catch(err){
+        if(err.response && err.response.status === 401){
+          return null
+        }
+        toast.error(err.response.data.message || "Something went wrong")
+      }
+    }
+  });
+
+  console.log("authuser", authUser )
+  if(isLoading) return null;
  
   return (
       <Layout> 
          <Routes>
-          <Route path="/" element= {<HomePage/>} />
-          <Route path="/signup" element= {<SignupPage/>} />
-          <Route path="/login" element= {<LoginPage/>} />
+          <Route path="/" element= {authUser ? <HomePage/>:  <Navigate to = {"/login"} />} />
+          <Route path="/signup" element= {!authUser ? <SignupPage/> : <Navigate to = {"/"} /> } />
+          <Route path="/login" element= {!authUser ? <LoginPage/>:  <Navigate to = {"/"} />} />
          </Routes>
          <Toaster/>
       </Layout>
